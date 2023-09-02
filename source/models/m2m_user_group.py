16830e65bd0787cb0b4f452b54d65e14774e36da
Alexander <aandryukov@more.tv>
2023-09-02 08:23:14 +0500

    code format

--- source/models/m2m_user_group.py
+++ source/models/m2m_user_group.py
@@ -11,4 +11,4 @@ class M2MUserGroup(Base):
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), index=True)
     group_id: Mapped[int] = mapped_column(ForeignKey("groups.id"), index=True)
 
-    __table_args__ = (Index('user_group_together', "user_id", "group_id"), )
+    __table_args__ = (Index("user_group_together", "user_id", "group_id"),)
